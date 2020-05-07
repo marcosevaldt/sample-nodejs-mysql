91340a655b2a74d17f7db8b0f5ab6710534196ab
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-07 10:20:24 -0300

    Configuração inicial database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,15 @@
 'use strict';
 
 // Constants
-const routes = require('express').Router();
+const routes = require('express').Router()
 
 // Routes
 routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Hello World!' })
 });
 
-routes.get('/database', (req, res) => {
-  const Sequelize = require('sequelize');
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  });
-  sequelize
-  .authenticate()
-  .then(() => {
-    res.status(200).json({ message: 'Connection has been established successfully.' });
-  })
-  .catch(err => {
-    res.status(200).json({ message: 'Unable to connect to the database:', error: err});
-  });
+routes.get('/database', async (req, res) => {
+  res.status(200).json({ message: 'database' })
 });
 
 // Export
