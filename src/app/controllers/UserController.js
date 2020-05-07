17a7c93c69f96a4f24c45629af4a3bd59c33c7de
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-07 13:03:17 -0300

    Identação da controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,13 +2,13 @@ const Model = require('../models');
 
 class UserController
 {
- async index(req, res)
- {
-  Model.users.findAll().then(data => {
+  async index(req, res)
+  {
+    Model.users.findAll().then(data => {
       return res.send(data);
     }).catch(err => {
       res.status(500).send({ message: err.message || "Some error occurred while retrieving users." });
     });
- }
+  }
 }
 module.exports = new UserController();
