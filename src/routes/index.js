f09781ee1b1a9fc42053d84a1d0aff108b635a9c
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-07 12:31:05 -0300

    Criando controller e mudando layer

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,25 +2,14 @@
 
 // Constants
 const routes = require('express').Router()
-const Model = require('../app/models');
+const UserController = require('../app/controllers/UserController');
 
 // Routes
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' })
 });
 
-routes.get('/database', async (req, res) => {
-  Model.users.findAll()
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving users."
-      });
-    });
-});
+routes.get('/users', UserController.index);
 
 // Export
 module.exports = routes;
