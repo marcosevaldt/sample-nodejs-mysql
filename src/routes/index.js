9ee8dcab70d588fa5ebb91d5c83a74757373ee38
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-07 08:35:25 -0300

    Adicionando simples conexão com banco mysql local

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,18 @@ routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
 
+routes.get('/database', (req, res) => {
+  const Sequelize = require('sequelize');
+  const sequelize = new Sequelize('mysql://root:root@mysql:3306/acao_reacao');
+  sequelize
+  .authenticate()
+  .then(() => {
+    res.status(200).json({ message: 'Connection has been established successfully.' });
+  })
+  .catch(err => {
+    res.status(200).json({ message: 'Unable to connect to the database:', error: err});
+  });
+});
+
 // Export
 module.exports = routes;
