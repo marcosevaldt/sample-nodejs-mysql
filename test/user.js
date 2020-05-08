1268320df277bb8bd882844fd7a8ab73e5bc4831
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-08 04:00:31 -0300

    Ajustando configs para testes funcionais

--- test/user.js
+++ test/user.js
@@ -1,10 +1,28 @@
 const assert = require('assert')
 const { users } = require('../src/app/models')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = chai.expect;
+chai.use(chaiHttp);
+
 describe('User', function() {
+
   it('it should create a user', async function() {
     const userObject = { name: 'Marcos', email: 'marcos@email.com' }
     const { name, email } = await users.create(userObject)
     const assertObject = { name: name, email: email }
     assert.deepEqual(userObject, assertObject)
   });
+
+  it('it should return users list with status code 200', async function() {
+    chai.request('http://0.0.0.0:8080')
+    .get('/users')
+    .then(function (res) {
+      expect(res).to.have.status(200);
+    })
+    .catch(function (err) {
+      throw err;
+    });
+  });
+
 });
