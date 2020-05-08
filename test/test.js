ad640af63cf684c664197fe84f6855b4381ddc0d
Marcos Evaldt Ereno <marcos.evaldt@uhuu.com>
2020-05-08 02:48:43 -0300

    Criando um teste valido

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const assert = require('assert')
+const { users } = require('../src/app/models')
+describe('User', function() {
+  it('it should create a user', async function() {
+    const userObject = { name: 'Marcos', email: 'marcos@email.com' }
+    const { name, email } = await users.create(userObject)
+    const assertObject = { name: name, email: email }
+    assert.deepEqual(userObject, assertObject)
   });
 });
